6fcc062edaafbc045dadfd00993da10649f85fca
pankaj.bhagchandani <pankaj.bhagchandani@team.telstra.com>
2016-09-27 11:56:03 +1000

    Added router support : Some code is copied

--- app/hero.service.js
+++ app/hero.service.js
@@ -8,15 +8,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
 var mock_heroes_1 = require('./mock-heroes');
+var core_1 = require('@angular/core');
 var HeroService = (function () {
     function HeroService() {
     }
     HeroService.prototype.getHeroes = function () {
         return Promise.resolve(mock_heroes_1.HEROES);
     };
-    ;
     HeroService.prototype.getHeroesSlowly = function () {
         var _this = this;
         return new Promise(function (resolve) {
@@ -24,7 +23,10 @@ var HeroService = (function () {
         }) // delay 2 seconds
             .then(function () { return _this.getHeroes(); });
     };
-    ;
+    HeroService.prototype.getHero = function (id) {
+        return this.getHeroes()
+            .then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
+    };
     HeroService = __decorate([
         core_1.Injectable(), 
         __metadata('design:paramtypes', [])
